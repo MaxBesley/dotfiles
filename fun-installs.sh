3272627f5a04e3564acf24c202c848240cfe973b
Max Besley <besleymax@gmail.com>
2024-04-14 22:07:10 +1000

    added a shell script

---
+++ fun-installs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt -y install \
+                 sl \
+           neofetch \
+            nyancat \
+             figlet \
+            fortune \
+             cowsay \
+             lolcat \
+            cmatrix \
+     pacman4console
